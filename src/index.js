64df18fae223ff6ee6e8cd154a5f97d5a9bd69ba
Micheal <167960784+micheal-r@users.noreply.github.com>
2024-04-27 23:17:35 +0200

    delete

--- src/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const Hexalon = require('./structures/client');
-const client = new Hexalon();
-client.connect(process.env.TOKEN)
-
-const { EventEmitter } = require('events');
-
-// Assuming 'bot' is your EventEmitter
-client.setMaxListeners(100); // Setting the limit to 30 listeners
