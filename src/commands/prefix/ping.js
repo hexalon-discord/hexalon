bf8ee0369694aa094a78b0bf43e84536f1705ed0
Micheal <r.micheal.dev@gmail.com>
2024-04-28 09:45:32 +0200

    Added ping command

---
+++ src/commands/prefix/ping.js
@@ -0,0 +1,21 @@
+const { EmbedBuilder, PermissionsBitField } = require("discord.js");
+
+module.exports = {
+  name: 'ping',
+  aliases: ['latency'],
+  description: 'Get the bot\'s latency stats',
+  staffOnly: false,
+  debugType: true,
+  callback: async (message, args, client, prefix, debug) => {
+    try {
+    const embedPing = new EmbedBuilder()
+    .setTitle('Hexalon latency')
+    .setColor(client.config.customization.embedColor)
+    .setDescription(`Pong! 🏓 \nCommand latency is ${Date.now() - message.createdTimestamp}ms. \nAPI latency is ${Math.round(client.ws.ping)}ms.`)
+    .setTimestamp();
+      message.reply({embeds: [embedPing]});
+    } catch (error) {
+      throw error;
+    }
+  }
+}
