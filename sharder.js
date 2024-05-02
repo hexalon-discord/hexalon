f99c0b90256b7ebab2a991d9001315768d42c1b5
Micheal <r.micheal.dev@gmail.com>
2024-05-02 21:05:29 +0200

    added sharding

---
+++ sharder.js
@@ -0,0 +1,12 @@
+const { ShardingManager } = require('discord.js');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const manager = new ShardingManager('./src/index.js', {
+  token: process.env.TOKEN,
+  totalShards: 1
+});
+
+manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+manager.spawn();
